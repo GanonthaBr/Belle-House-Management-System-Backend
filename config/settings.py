afb3271a4676e0a24fc6c89dffdef0d2b892f0a0
= <bganontha@gmail.com>
2025-12-20 16:07:15 +0200

    added allowed domains to allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -212,7 +212,7 @@ SIMPLE_JWT = {
 
 CORS_ALLOWED_ORIGINS = config(
     'CORS_ALLOWED_ORIGINS',
-    default='http://localhost:3000,http://127.0.0.1:3000,https://api2.bellehouseniger.com,https://bellehouseniger.com',
+    default='http://localhost:3000,http://127.0.0.1:3000,https://api2.bellehouseniger.com,https://bellehouseniger.com,51.91.159.155',
     cast=Csv()
 )
 CORS_ALLOW_CREDENTIALS = True
